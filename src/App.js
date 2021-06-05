ad2821e91300cef44cc503b4aec4bbf40c051092
Choijun0 <73348238+choijun0@users.noreply.github.com>
2021-06-05 14:28:38 +0900

    <React-Hook>useInput Make useInput function to control event in function.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 
+const useIput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 const App = () => {
-  return <div></div>;
+  const maxlen = (inputString) => inputString.length <= 10;
+  const name = useIput("Mr.", maxlen);
+  return (
+    <div>
+      <h1>Hello</h1>
+      <input placeholder="Name" value={name.value} onChange={name.onChange} />
+    </div>
+  );
 };
 
 export default App;
