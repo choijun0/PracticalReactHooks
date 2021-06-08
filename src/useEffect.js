77140609ad2a1840c17e9fdb4a85ffd5c9e98547
Choijun0 <73348238+choijun0@users.noreply.github.com>
2021-06-08 19:02:12 +0900

    <React-Hook>useEffect(useTitle)

--- src/useEffect.js
+++ src/useEffect.js
@@ -1,23 +1,18 @@
 import React, { useState, useEffect } from "react";
 import ReactDom from "react-dom";
-//useEffect
-//first element: function
-//second element: [dependencies]
-//basically do componentDidMount, componentDidUpdate, componentWillUnmount
-//can target variables by add second element
-//if do not set second element, all changes call function.
-//[] - respond nothing
 
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const titleUpdate = () => {
+    const htmlTitle = document.querySelector("title"); //<title></title> object
+    htmlTitle.innerText = title;
+  };
+  useEffect(titleUpdate, [title]);
+  return setTitle;
+};
 const App = () => {
-  const [number, setNumber] = useState(0);
-  const [anumber, setAnumber] = useState(0);
-  const say = () => console.log("change emerged");
-  useEffect(say, [number, anumber]);
-  return (
-    <div className="App">
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(anumber + 1)}>{anumber}</button>
-    </div>
-  );
+  const titleUpdator = useTitle("Loading...");
+  setTimeout(() => titleUpdator("Home"), 5000);
+  return <div>Hi</div>;
 };
 export default App;
